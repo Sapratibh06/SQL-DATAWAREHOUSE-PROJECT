4dc5c7b74e859b86c2858258f9793c544c58ff84
Sapratibh Das <sapratibh06@gmail.com>
2026-09-10 19:02:30 +0530

    Initialize DataWarehouse database and schemas

---
+++ scripts/init_database.sql
@@ -0,0 +1,13 @@
+---Create Database 'DataWarehouse'--
+
+use master;
+
+Create Database Datawarehouse;
+
+use datawarehouse;
+
+Create SCHEMA bronze;
+go
+Create SCHEMA silver;
+go
+Create SCHEMA gold;
